0a91730c495acf8bc3b081e09eacee77c4783a73
Tom Sheppard <sheppardnexus@gmail.com>
2021-09-12 12:59:46 +0100

    update email api

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,14 +1,16 @@
 const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async event => {
     const payload = JSON.parse(event.body).payload.data;
     const referrer = new URL(payload.referrer);
     if (referrer.pathname === "/contact/join/") {
+        const branchArr = payload.branch[0].split(",");
         try {
-            const fullName = payload.fullname;
-            const pronouns = payload.pronouns;
-            const email = payload.email;
-            const branchArr = payload.branch[0].split(",");
+            const volunteerEmail = payload.email;
+            const volunteerName = payload.fullname;
+            const volunteerPronouns = payload.pronouns;
+
             const branchEmail = branchArr[0];
             const branchName = branchArr[1];
 
@@ -18,21 +20,21 @@ exports.handler = async event => {
                 to: `${branchEmail}`,
                 from: `${sender}`,
                 subject: "You have a new volunteer!",
-                text: `${fullName} wants to join your branch.`,
+                text: `${volunteerName} wants to join your branch.`,
                 html: `
                 <div>
-                    <p><strong><i>${fullName}</i></strong> (${pronouns})</p>
-                    <p><a href="mailto:${email}">${email}</a></p>
+                    <p><strong><i>${volunteerName}</i></strong> (${volunteerPronouns})</p>
+                    <p><a href="mailto:${volunteerEmail}">${volunteerEmail}</a></p>
                 </div>
                 <div>
-                    <p><i>${fullName}</i> wants to join <i>Sexpression:${branchName}</i>. Introduce yourself by sending them an email. Invite them to your social media and make sure they feel welcomed! 🌈</p>
+                    <p><i>${volunteerName}</i> wants to join <i>Sexpression:${branchName}</i>. Introduce yourself by sending them an email. Invite them to your social media and make sure they feel welcomed! 🌈</p>
                 </div>`,
             };
 
             console.log(JSON.stringify({ msg: msg }));
 
-            sgMail.setApiKey(process.env.SENDGRID_API_KEY);
             let response = await sgMail.send(msg);
+            console.log("Email sent");
             return response;
         } catch (error) {
             console.error(error);
