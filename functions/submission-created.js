4b240b8a49519ba442f342eb4f6a853cd65f0b9e
Tom Sheppard <sheppardnexus@gmail.com>
2021-09-16 22:25:59 +0100

    See if this works

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,80 +1,61 @@
-const sgMail = require('@sendgrid/mail');
+const client = require('@sendgrid/mail');
 
-exports.handler = async event => {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const {
+    SENDGRID_API_KEY,
+    SENDGRID_FROM_EMAIL,
+} = process.env;
+
+exports.handler = async function (event, context, callback) {
     const payload = JSON.parse(event.body).payload.data;
-    const referrer = new URL(payload.referrer);
-    if (referrer.pathname === "/contact/join/") {
 
-        try {
-            const volunteerEmail = payload.email;
-            const volunteerName = payload.fullname;
-            const volunteerPronouns = payload.genderpronouns;
+    const volunteerEmail = payload.email;
+    const volunteerName = payload.fullname;
+    const volunteerPronouns = payload.genderpronouns;
 
-            if (volunteerPronouns == null) {
-                volunteerPronouns = 'They/Them/Their'
-            }
-            
-            const branchArr = payload.branch.split(",");
-            const branchEmail = branchArr[0];
-            const branchName = branchArr[1];
+    if (volunteerPronouns == null) {
+        volunteerPronouns = 'They/Them/Their'
+    }
 
-            const sender = "website@sexpression.org.uk"
+    const branchArr = payload.branch.split(",");
+    const branchEmail = branchArr[0];
+    const branchName = branchArr[1];
 
-            const msgToBranch = {
-                to: `${branchEmail}`,
-                from: `${sender}`,
-                subject: "You have a new volunteer!",
-                text: `${volunteerName} wants to join your branch.`,
-                html: `
-                <div>
-                    <p><strong><i>${volunteerName}</i></strong> (${volunteerPronouns})</p>
-                    <p><a href="mailto:${volunteerEmail}">${volunteerEmail}</a></p>
-                </div>
-                <div>
-                    <p><i>${volunteerName}</i> wants to join <i>Sexpression:${branchName}</i>. Introduce yourself by sending them an email. Invite them to your social media and make sure they feel welcomed! 🌈</p>
-                </div>`,
-            };
+    client.setApiKey(SENDGRID_API_KEY);
 
-            const msgToVolunteer = {
-                to: `${volunteerEmail}`,
-                from: `${sender}`,
-                subject: `Thanks for reaching out to Sexpression:${branchName}`,
-                text: `The branch committee memeber of Sexpression:${branchName} will reach out to you shortly.`,
-            };
-
-            // let response = await sgMail.send(msg);
-            // console.log("Email sent");
-            // return response;
-
-            console.log('Got here');
-            sgMail.send(msgToBranch).then(() => {
-                console.log('Branch email: SUCCESS');
-            }).catch((error) => {
-                console.log('Branch email: FAIL');
-                console.error(error);
-            })
-
-            sgMail.send(msgToVolunteer).then(() => {
-                console.log('Volunteer email: SUCCESS');
-            }).catch((error) => {
-                console.log('Volunteer email: FAIL');
-                console.error(error);
-            })
-
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                  message: 'Emails sent'
-                })
-              };
-            
-        } catch (error) {
-            console.error(error);
-
-            if (error.response) {
-                console.error(error.response.body)
-            }
-        }
+    const msgToBranch = {
+        to: `${branchEmail}`,
+        from: SENDGRID_FROM_EMAIL,
+        subject: "You have a new volunteer!",
+        text: `${volunteerName} wants to join your branch.`,
+        html: `
+        <div>
+            <p><strong><i>${volunteerName}</i></strong> (${volunteerPronouns})</p>
+            <p><a href="mailto:${volunteerEmail}">${volunteerEmail}</a></p>
+        </div>
+        <div>
+            <p><i>${volunteerName}</i> wants to join <i>Sexpression:${branchName}</i>. Introduce yourself by sending them an email. Invite them to your social media and make sure they feel welcomed! 🌈</p>
+        </div>`,
     };
+
+    const msgToVolunteer = {
+        to: `${volunteerEmail}`,
+        from: SENDGRID_FROM_EMAIL,
+        subject: `Thanks for reaching out to Sexpression:${branchName}`,
+        text: `The branch committee memeber of Sexpression:${branchName} will reach out to you shortly.`,
+    };
+
+    try {
+        await client.send(msgToBranch);
+        await client.send(msgToVolunteer);
+        return {
+            statusCode: 200,
+            body: 'Messages sent',
+        };
+    } catch (err) {
+        return {
+            statusCode: err.code,
+            body: JSON.stringify({ msg: err.message }),
+        };
+    }
+
 };
