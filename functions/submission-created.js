95f45fb07e0bbf5a8417a5d47d85c5e8239aeb00
Tom Sheppard <sheppardnexus@gmail.com>
2021-09-23 20:13:14 +0100

    new submission function

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,47 +1,63 @@
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const client = require('@sendgrid/mail');
 
-exports.handler = async event => {
+const {
+    SENDGRID_API_KEY,
+    SENDGRID_FROM_EMAIL,
+} = process.env;
+
+exports.handler = async function (event, context, callback) {
     const payload = JSON.parse(event.body).payload.data;
-    const referrer = new URL(payload.referrer);
-    if (referrer.pathname === "/contact/join/") {
-        const branchArr = payload.branch[0].split(",");
-        try {
-            const volunteerEmail = payload.email;
-            const volunteerName = payload.fullname;
-            const volunteerPronouns = payload.pronouns;
 
-            const branchEmail = branchArr[0];
-            const branchName = branchArr[1];
+    console.log('dog');
 
-            const sender = "website@sexpression.org.uk"
+    const volunteerEmail = payload.email;
+    const volunteerName = payload.fullname;
+    const volunteerPronouns = payload.genderpronouns;
 
-            const msg = {
-                to: `${branchEmail}`,
-                from: `${sender}`,
-                subject: "You have a new volunteer!",
-                text: `${volunteerName} wants to join your branch.`,
-                html: `
-                <div>
-                    <p><strong><i>${volunteerName}</i></strong> (${volunteerPronouns})</p>
-                    <p><a href="mailto:${volunteerEmail}">${volunteerEmail}</a></p>
-                </div>
-                <div>
-                    <p><i>${volunteerName}</i> wants to join <i>Sexpression:${branchName}</i>. Introduce yourself by sending them an email. Invite them to your social media and make sure they feel welcomed! 🌈</p>
-                </div>`,
-            };
+    if (volunteerPronouns == null) {
+        volunteerPronouns = 'They/Them/Their'
+    }
 
-            console.log(JSON.stringify({ msg: msg }));
+    const branchArr = payload.branch.split(",");
+    const branchEmail = branchArr[0];
+    const branchName = branchArr[1];
 
-            let response = await sgMail.send(msg);
-            console.log("Email sent");
-            return response;
-        } catch (error) {
-            console.error(error);
+    console.log('cat');
 
-            if (error.response) {
-                console.error(error.response.body)
-            }
-        }
+    client.setApiKey(SENDGRID_API_KEY);
+
+    console.log('mouse');
+
+    const msgToBranch = {
+        to: `${branchEmail}`,
+        from: SENDGRID_FROM_EMAIL,
+        subject: "You have a new volunteer!",
+        text: `${volunteerName} wants to join your branch.`,
+        html: `<div>🌈</div>`,
     };
-};
+    console.log('fish');
+
+    // const msgToVolunteer = {
+    //     to: `${volunteerEmail}`,
+    //     from: SENDGRID_FROM_EMAIL,
+    //     subject: `Thanks for reaching out to Sexpression:${branchName}`,
+    //     text: `The branch committee memeber of Sexpression:${branchName} will reach out to you shortly.`,
+    // };
+
+
+    console.log('tiger');
+
+    let response1 = await client.send(msgToBranch);
+
+    console.log('frog');
+
+    //    let response2 = await client.send(msgToVolunteer);
+
+    console.log(response1);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ msg: response1 }),
+    };
+
+}
