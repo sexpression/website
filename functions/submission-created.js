9623d6ba8fb7d1c4724955ec1d04a296628f09d3
Tom Sheppard <sheppardnexus@gmail.com>
2021-09-24 21:10:55 +0100

    double email

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -7,9 +7,6 @@ const {
 
 exports.handler = async function (event, context, callback) {
     const payload = JSON.parse(event.body).payload.data;
-
-    console.log('dog');
-
     const volunteerEmail = payload.email;
     const volunteerName = payload.fullname;
     const volunteerPronouns = payload.genderpronouns;
@@ -22,42 +19,35 @@ exports.handler = async function (event, context, callback) {
     const branchEmail = branchArr[0];
     const branchName = branchArr[1];
 
-    console.log('cat');
-
     client.setApiKey(SENDGRID_API_KEY);
-
-    console.log('mouse');
 
     const msgToBranch = {
         to: `${branchEmail}`,
         from: SENDGRID_FROM_EMAIL,
-        subject: "You have a new volunteer!",
-        text: `${volunteerName} wants to join your branch.`,
-        html: `<div>🌈</div>`,
+        subject: "New volunteer - ${volunteerName} 🎉",
+        text: `${volunteerName} (${volunteerPronouns}) wants to join your branch.`,
     };
-    console.log('fish');
 
-    // const msgToVolunteer = {
-    //     to: `${volunteerEmail}`,
-    //     from: SENDGRID_FROM_EMAIL,
-    //     subject: `Thanks for reaching out to Sexpression:${branchName}`,
-    //     text: `The branch committee memeber of Sexpression:${branchName} will reach out to you shortly.`,
-    // };
-
-
-    console.log('tiger');
+    const msgToVolunteer = {
+        to: `${volunteerEmail}`,
+        from: SENDGRID_FROM_EMAIL,
+        subject: `Thank you from Sexpression:${branchName} 💖`,
+        text: `Sexpression:${branchName} will reach out to you shortly.`,
+    };
 
     let response1 = await client.send(msgToBranch);
-
-    console.log('frog');
-
-    //    let response2 = await client.send(msgToVolunteer);
-
-    console.log(response1);
+    let response2 = await client.send(msgToVolunteer);
 
     return {
         statusCode: 200,
-        body: JSON.stringify({ msg: response1 }),
+        body: JSON.stringify(
+            { 
+                msg: {
+                    branch: response1,
+                    volunteer: response2
+                }
+            }
+        ),
     };
 
 }
