e02d7e7507a8167dfd0da7000b9f7330ce82643a
Tom Sheppard <sheppardnexus@gmail.com>
2022-02-06 21:00:41 +0000

    updated resources

--- src/_data/resources.js
+++ src/_data/resources.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+const moment = require('moment');
 
 const domain = "https://sexpression.org.uk";
 const path = "/.netlify/functions/resources";
@@ -8,5 +9,14 @@ const url = new URL(path, domain);
 module.exports = async function () {
     const response = await fetch(url);
     const jsonResponse = await response.json();
-    return jsonResponse.records;
+    const results = jsonResponse.records;
+
+    results.forEach(function (record, i) {
+        let formattedDate = moment().format(record.fields["Last updated"]); 
+        // const date = hdate.prettyPrint(record.fields.Las§tUpdated);
+        // record.fields.Date = date;
+        results[i].fields["Last updated"] = formattedDate;
+    });
+
+    return results;
 };
