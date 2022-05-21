01e17a329959bce216a44476d3bf96251ce6b5d2
Tom Sheppard <sheppardnexus@gmail.com>
2022-05-21 14:53:23 +0100

    function update

--- functions/countries.js
+++ functions/countries.js
@@ -7,19 +7,20 @@ const baseView = "Active universities";
 const baseField = "Country";
 
 console.log({ apiKey: `${AIRTABLE_API_KEY}`, base: `${AIRTABLE_BASE_ID}` });
-
-exports.handler = function (event, context, callback) {
+console.log('dogs')
+exports.handler = function(event, context, callback) {
     try {
         base(baseName).select({
             view: baseView,
             fields: [baseField],
             sort: [{ field: baseField, direction: "asc" }]
-        }).firstPage(function (err, records) {
+        }).firstPage(function(err, records) {
+            console.log('cats')
             if (err) { console.error(err); return; }
 
             const obj = { records: [] };
             let last = "test";
-            records.forEach(function (record) {
+            records.forEach(function(record) {
                 let country = record.fields.Country;
                 if (country != last) {
                     let newbie = { name: country };
@@ -27,7 +28,7 @@ exports.handler = function (event, context, callback) {
                     obj.records.push(newbie);
                 }
             });
-
+            console.log('monkey')
             callback(null, {
                 statusCode: 200,
                 headers: {
