ee2f5026ae047d731cfb0ed61210d95d691aa27e
Tom Sheppard <sheppardnexus@gmail.com>
2022-06-04 16:41:20 +0100

    New programme

--- src/_data/programme.js
+++ src/_data/programme.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+const markdown = require("markdown").markdown;
 
 const domain = "https://sexpression.org.uk";
 const path = "/.netlify/functions/programme";
@@ -7,5 +8,9 @@ const url = new URL(path, domain);
 module.exports = async function() {
     const response = await fetch(url);
     const jsonResponse = await response.json();
+    jsonResponse.items.forEach(element => {
+        element.body = markdown.toHTML(element.body)
+    });
+
     return jsonResponse.items;
 };
