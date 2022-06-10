53c84830626523e6e2bbad5503fe9e8fca31bc2c
Tom Sheppard <sheppardnexus@gmail.com>
2022-06-10 22:26:58 +0100

    trustees actually added

--- functions/trustees.js
+++ functions/trustees.js
@@ -1,29 +1,32 @@
-const Airtable = require('airtable');
-const { AIRTABLE_BASE_ID, AIRTABLE_API_KEY } = process.env;
-var base = new Airtable({ apiKey: `${AIRTABLE_API_KEY}` }).base(`${AIRTABLE_BASE_ID}`);
+// import { Directus } from '@directus/sdk';
+const { Directus } = require('@directus/sdk');
+const directus = new Directus('https://zq5bmezp.directus.app');
 
-exports.handler = function(event, context, callback) {
+const table = 'trustees';
+// const fields = ['*', 'university.country', 'university.name'];
+
+exports.handler = async function(event, context) {
     try {
-        base('Members').select({
-            view: "Trusteeship",
-            filterByFormula: "NOT({Live} = '')",
-            sort: [{ field: "Name", direction: "asc" }]
-        }).firstPage(function(err, records) {
-            if (err) { console.error(err); return; }
+        const data = await directus.items(table).readByQuery({ meta: 'total_count' });
 
-            callback(null, {
-                statusCode: 200,
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ records })
-            });
-        });
+        //fields('university.name')
+
+        console.log("successful!");
+
+        return {
+            statusCode: 200,
+            message: "All good in the hood",
+            body: JSON.stringify({
+                items: data.data,
+                total: data.meta.total_count,
+            })
+        }
+
     } catch (err) {
-        console.log(err) // output to netlify function log
+        console.log(JSON.stringify({ msg: err.message }));
         return {
             statusCode: 500,
-            body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
+            message: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
         }
     }
 }
