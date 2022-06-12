1194c30669a4f908f29dfeda1fdd8a64169edb60
Tom Sheppard <sheppardnexus@gmail.com>
2022-06-12 21:25:20 +0100

    new branches

--- functions/branches.js
+++ functions/branches.js
@@ -3,10 +3,56 @@ const directus = new Directus('https://zq5bmezp.directus.app');
 
 const table = 'branches';
 const fields = ['*', 'university.country', 'university.name'];
+const filter = { "status": { "_eq": "published" } };
+
+function queryFormatter(string) {
+    string.replaceAll('-', ' ');
+    let splitStr = string.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(' ');
+}
+
+function updateFilterStatus(string) {
+    if (string) {
+        filter.status = { "_eq": queryStatus };
+    }
+}
+
+function updateFilterCountry(string) {
+    if (string) {
+        let country = {
+            "university": {
+                "country": {
+                    "_eq": string
+                }
+            }
+        }
+        Object.assign(filter, country);
+    }
+}
 
 exports.handler = async function(event, context) {
+
+    let queryStatus = event.queryStringParameters.status;
+    let queryCountry = event.queryStringParameters.country;
+
+    if (queryStatus) {
+        let item = queryFormatter(queryStatus)
+        updateFilterStatus(item);
+    }
+
+    if (queryCountry) {
+        let item = queryFormatter(queryCountry)
+        updateFilterCountry(item)
+    }
+
+    console.log(filter);
+
     try {
-        const data = await directus.items(table, fields).readByQuery({ meta: 'total_count', fields: fields });
+        const data = await directus.items(table).readByQuery({ meta: 'total_count', fields: fields, filter: filter });
+
         console.log("successful!");
 
         return {
