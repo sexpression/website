040be9b3a4f5e50bc47af42529987aca062c1d11
Tom Sheppard <sheppardnexus@gmail.com>
2022-06-18 15:08:29 +0100

    Bug fix

--- functions/universities.js
+++ functions/universities.js
@@ -15,7 +15,7 @@ function queryFormatter(string) {
 }
 
 function updateFilterStatus(string) {
-    filter.status = { "_eq": queryStatus };
+    filter.status = { "_eq": string };
 }
 
 function updateFilterCountry(string) {
