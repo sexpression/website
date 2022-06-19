e3566f4757d17b155d743b0073ca7584d2861fe7
Tom Sheppard <sheppardnexus@gmail.com>
2022-06-19 20:25:51 +0100

    submit home

---
+++ functions/home.js
@@ -0,0 +1,29 @@
+const { DIRECTUS_URL } = process.env;
+const { Directus } = require('@directus/sdk');
+const directus = new Directus(`https://${DIRECTUS_URL}`);
+
+const table = 'home';
+
+exports.handler = async function(event, context) {
+    try {
+        const data = await directus.items(table).readByQuery({ meta: 'total_count' });
+
+        console.log("successful!");
+
+        return {
+            statusCode: 200,
+            message: "All good in the hood",
+            body: JSON.stringify({
+                items: data.data,
+                total: data.meta.total_count,
+            })
+        }
+
+    } catch (err) {
+        console.log(JSON.stringify({ msg: err.message }));
+        return {
+            statusCode: 500,
+            message: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
+        }
+    }
+}
