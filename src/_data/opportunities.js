b1e27e28ca2da05973dd7ce34b360bc05c44c661
Tom Sheppard <sheppardnexus@gmail.com>
2022-06-21 21:31:13 +0100

    opportunities

--- src/_data/opportunities.js
+++ src/_data/opportunities.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+const markdown = require("markdown").markdown;
 
 const domain = "https://sexpression.org.uk";
 const path = "/.netlify/functions/opportunities";
@@ -8,5 +9,8 @@ const url = new URL(path, domain);
 module.exports = async function() {
     const response = await fetch(url);
     const jsonResponse = await response.json();
+    jsonResponse.items.forEach(element => {
+        element.body = markdown.toHTML(element.body)
+    });
     return jsonResponse.items;
 };
