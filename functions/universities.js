3a5b12b7167257021516b162af0642de5f34ab53
Tom Sheppard <sheppardnexus@gmail.com>
2022-06-23 12:39:41 +0100

    universities fixed

--- functions/universities.js
+++ functions/universities.js
@@ -3,45 +3,63 @@ const { Directus } = require('@directus/sdk');
 const directus = new Directus(`https://${DIRECTUS_URL}`);
 
 const table = 'universities';
-const filter = { "status": { "_eq": "published" } };
-
-function queryFormatter(string) {
-    string.replace('-', / /g);
-    let splitStr = string.toLowerCase().split(' ');
-    for (let i = 0; i < splitStr.length; i++) {
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-    }
-    return splitStr.join(' ');
-}
-
-function updateFilterStatus(string) {
-    filter.status = { "_eq": string };
-}
-
-function updateFilterCountry(string) {
-    let country = {
-        "country": {
-            "_eq": string
-        }
-    }
-    Object.assign(filter, country);
-}
 
 exports.handler = async function(event, context) {
     try {
+
+        let fields = ['*', 'branches.country', 'university.name'];
+        let filter = { "status": { "_eq": "published" } };
+
+        function queryFormatterCaps(string) {
+            // WAlEs
+            // Wales
+            string.replace('-', / /g);
+            let splitStr = string.toLowerCase().split(' ');
+            for (let i = 0; i < splitStr.length; i++) {
+                splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+            }
+            return splitStr.join(' ');
+        }
+
+        function queryFormatterNoCaps(string) {
+            // ARChIVeD
+            // archived
+            string.replace('-', / /g);
+            let splitStr = string.toLowerCase().split(' ');
+            for (let i = 0; i < splitStr.length; i++) {
+                splitStr[i] = splitStr[i].charAt(0) + splitStr[i].substring(1);
+            }
+            return splitStr.join(' ');
+        }
+
+        function updateFilterStatus(string) {
+            console.log(string)
+            filter.status = { "_eq": string };
+        }
+
+        function updateFilterCountry(string) {
+            console.log(string)
+            let country = {
+                "country": {
+                    "_eq": string
+                }
+            }
+            Object.assign(filter, country);
+        }
+
         let queryStatus = event.queryStringParameters.status;
         let queryCountry = event.queryStringParameters.country;
 
         if (queryStatus) {
-            updateFilterStatus(queryFormatter(queryStatus));
+            updateFilterStatus(queryFormatterNoCaps(queryStatus));
         }
 
         if (queryCountry) {
-            updateFilterCountry(queryFormatter(queryCountry))
+            updateFilterCountry(queryFormatterCaps(queryCountry))
         }
 
         console.log(filter);
-        const data = await directus.items(table).readByQuery({ meta: 'total_count', filter: filter });
+        let data = await directus.items(table).readByQuery({ meta: 'total_count', fields: fields, filter: filter });
 
         console.log("successful!");
 
