30318493e8f483d7450da89a5a9ab51a8904975b
Tom Sheppard <sheppardnexus@gmail.com>
2022-06-30 22:33:23 +0100

    pretty deadline

--- src/_data/opportunities.js
+++ src/_data/opportunities.js
@@ -1,4 +1,5 @@
 const markdown = require("markdown").markdown;
+const moment = require('moment');
 const { Directus } = require('@directus/sdk');
 
 const { DIRECTUS_URL } = process.env;
@@ -12,6 +13,16 @@ function prettyMarkdown(data) {
     return data;
 }
 
+function prettyDeadline(data) {
+    for (let element in data) {
+        let stat = element.deadline;
+        let deadline = moment(stat).format("MMMM Do YYYY, h:mma");
+        data[element].deadline = deadline;
+    };
+
+    return data;
+}
+
 module.exports = async function() {
     try {
         let table = 'opportunities';
@@ -20,6 +31,7 @@ module.exports = async function() {
         let meta = 'total_count';
 
         let response = await directus.items(table).readByQuery({ meta: meta, sort: sort, filter: filter });
+        response.data = prettyDeadline(response.data)
         response.data = prettyMarkdown(response.data);
         return response.data
 
