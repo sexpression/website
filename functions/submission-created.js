dca96e5092532591a1f4ede57f732bc1920c2d63
Tom Sheppard <sheppardnexus@gmail.com>
2022-07-07 21:25:06 +0100

    Updated emailing functionality

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -44,90 +44,91 @@ async function emailSender(msg) {
 }
 
 exports.handler = async function(event, context, callback) {
-    let payload = JSON.parse(event.body).payload.data;
-    let path = event.path.slice(0, -1).substring(event.path.slice(0, -1).lastIndexOf('/') + 1);
+    try {
+        let payload = JSON.parse(event.body).payload.data;
+        let meta = 'total_count';
+        let filter = { "template": { "_eq": payload.template } };
+        let fields = ['*', 'recipient.*'];
+        let form = await directus.items("forms").readByQuery({ meta: meta, filter: filter, fields: fields });
+        delete payload.user_agent;
+        delete payload.referrer;
+        delete payload.ip;
+        delete payload.template;
 
+        let template1 = {
+            '<>': 'div',
+            'html': [
+                { '<>': 'p', 'text': '${statement}' },
+                {
+                    '<>': 'ul',
+                    'html': [
+                        { '<>': 'li', 'text': '${key}: ${value}' },
+                    ]
+                }
+            ]
+        };
 
-    let meta = 'total_count';
-    let filter = { "template": { "_eq": payload.template } };
-    let fields = ['*', 'recipient.*'];
-    let form = await directus.items("forms").readByQuery({ meta: meta, filter: filter, fields: fields });
-    delete payload.user_agent;
-    delete payload.referrer;
-    delete payload.ip;
-    delete payload.template;
-
-    let template1 = {
-        '<>': 'div',
-        'html': [
-            { '<>': 'p', 'text': '${statement}' },
-            {
+        let template2 = {
+            '<>': 'div',
+            'html': [{
                 '<>': 'ul',
                 'html': [
                     { '<>': 'li', 'text': '${key}: ${value}' },
                 ]
+            }]
+        };
+
+        let data = [];
+
+        for (let prop in payload) {
+            if (Object.prototype.hasOwnProperty.call(payload, prop)) {
+                data.push({ 'key': prop, 'value': payload[prop] })
             }
-        ]
-    };
-
-    let template2 = {
-        '<>': 'div',
-        'html': [{
-            '<>': 'ul',
-            'html': [
-                { '<>': 'li', 'text': '${key}: ${value}' },
-            ]
-        }]
-    };
-
-    let data = [];
-
-    for (let prop in payload) {
-        if (Object.prototype.hasOwnProperty.call(payload, prop)) {
-            data.push({ 'key': prop, 'value': payload[prop] })
         }
-    }
 
-    // EMAIL 1 - SENDER //////////////////////////////////////////////////
-    try {
-        let copyData = JSON.parse(JSON.stringify(data));
-        copyData.unshift({ 'key': 'Statment', 'value': await form.data[0].response });
-        let html = htmlConstructor(copyData, template1);
-        let msg = emailConstructor(payload["Email"], email, `Thank you ${payload['Full name']}`, html)
-        await emailSender(msg);
-    } catch (error) {
-        console.error(error);
-    }
-
-    // EMAIL 2 - MEMBER //////////////////////////////////////////////////
-    try {
-        for (let x of await form.data[0].recipient) {
-            let html = htmlConstructor(data, template2);
-            let member = await directus.items("members").readByQuery({ meta: 'total_count', filter: { "id": { "_eq": x.members_id } }, fields: ['*', 'role.*'] });
-            let msg = emailConstructor(member.data[0].role.email, email, `New response | ${path}`, html);
-            await emailSender(msg);
-        }
-    } catch (error) {
-        console.error(error);
-    }
-
-    // EMAIL 3 - BRANCH //////////////////////////////////////////////////
-    if (path === "join-a-branch" || path === "request-a-session") {
+        // EMAIL 1 - SENDER //////////////////////////////////////////////////
         try {
-            let branchArr = payload["Branch"].split(",");
-            payload["Branch"] = branchArr[1];
-            let html = htmlConstructor(data, template2);
-            let msg = emailConstructor(branchArr[0], email, `New response | ${path}`, html)
+            let copyData = JSON.parse(JSON.stringify(data));
+            copyData.unshift({ 'key': 'Statment', 'value': await form.data[0].response });
+            let html = htmlConstructor(copyData, template1);
+            let msg = emailConstructor(payload["Email"], email, `Thank you ${payload['Full name']}`, html)
             await emailSender(msg);
         } catch (error) {
             console.error(error);
         }
-    } else {
-        console.log("branch skipped");
-    }
 
-    return {
-        statusCode: 200,
-        body: "successful mate",
-    };
+        // EMAIL 2 - MEMBER //////////////////////////////////////////////////
+        try {
+            for (let x of await form.data[0].recipient) {
+                let html = htmlConstructor(data, template2);
+                let member = await directus.items("members").readByQuery({ meta: 'total_count', filter: { "id": { "_eq": x.members_id } }, fields: ['*', 'role.*'] });
+                let msg = emailConstructor(member.data[0].role.email, email, `New response | ${form.data[0].title}`, html);
+                await emailSender(msg);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+
+        // EMAIL 3 - BRANCH //////////////////////////////////////////////////
+        if (payload["Branch"]) {
+            try {
+                let branchArr = payload["Branch"].split(",");
+                payload["Branch"] = branchArr[1];
+                let html = htmlConstructor(data, template2);
+                let msg = emailConstructor(branchArr[0], email, `New response | ${form.data[0].title}`, html)
+                await emailSender(msg);
+            } catch (error) {
+                console.error(error);
+            }
+        } else {
+            console.log("Branch email skipped");
+        }
+
+        return {
+            statusCode: 200,
+            body: "successful mate",
+        };
+    } catch (error) {
+        console.error(error);
+    }
 }
