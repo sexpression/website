82c0f7105dfb9f8ca5ac30a28fc994f3326b3725
Tom Sheppard <sheppardnexus@gmail.com>
2022-07-23 12:32:24 +0100

    Sitemap and robots autogenerated

--- .eleventy.js
+++ .eleventy.js
@@ -10,8 +10,6 @@ function eleventy(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/img");
     eleventyConfig.addPassthroughCopy("src/svg");
-    eleventyConfig.addPassthroughCopy("src/sitemap.xml");
-    eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy({ "src/css": "/" });
     eleventyConfig.addPassthroughCopy({
         "./src/admin/config.yml": "./admin/config.yml",
